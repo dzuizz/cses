c80395e1da2494ef6e8613599a2c47480a4eddc5
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-03-12 23:13:57 +0800

    Solved 1095 - Exponentiation w/ Recursive Binary Exponentiation Approach

--- 1095-Exponentiation.cpp
+++ 1095-Exponentiation.cpp
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int m=1e9+7;
-int binpow(int a,int b){
-  int ret=1;
-  while(b){
-    if(b&1)ret=ret*a%m;
-    a*=a,a%=m,b>>=1;
-  }
-  return ret;
+int binpow(int a, int b, int m){
+  if(b==0) return 1;
+  if(b==1) return a;
+  int tmp=binpow(a, b>>1, m);
+  tmp = tmp * tmp % m;
+  if(b&1) tmp = tmp * a % m;
+  return tmp;
 }
 signed main() {
   int n; cin>>n;
   for(int i=0;i<n;++i){
     int a,b; cin>>a>>b;
-    cout<<binpow(a,b)<<'\n';
+    cout<<binpow(a,b,1e9+7)<<'\n';
   }
 }
 
