1de6f5439372d52f6da7d13834e019e4d138ee7a
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-03-20 10:47:05 +0800

    Solved Course Schedule

--- 1679-Course_Schedule.cpp
+++ 1679-Course_Schedule.cpp
@@ -1,54 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-vector<int> adj[100005];
-bool vis[100005],cycle_hellyea;
-int col[100005];
-stack<int> st;
-void dfs(int i){
-	vis[i] = 1;
-	for(auto&x:adj[i]){
-		if(vis[x]) continue;
-		dfs(x);
-	}
-	st.emplace(i);
-}
-bool dfs_cycle(int i){
-	col[i]=1;
-	for(auto&x:adj[i]){
-		if(col[x]==0){
-			if(dfs_cycle(x)) return 1;
-		}else if(col[x]==1){
-			cycle_hellyea=1;
-			return 1;
-		}
-	}
-	col[i]=2;
-	return 0;
+vector<int> topo_sort(int n, vector<vector<int>> &adj) {
+    vector<int> in(n, 0), res;
+    for (auto &v : adj) for (int u : v) in[u]++;
+    queue<int> q;
+    for (int i = 0; i < n; i++) if (!in[i]) q.push(i);
+    while (!q.empty()) {
+        int v = q.front(); q.pop(); res.push_back(v);
+        for (int u : adj[v]) if (--in[u] == 0) q.push(u);
+    }
+    return res.size() == n ? res : vector<int>();
 }
 signed main(){
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n,m; cin>>n>>m;
-	for(int i=0;i<m;++i){
-		int a,b; cin>>a>>b;
-		adj[a].emplace_back(b);
-	}
-	for(int i=1;i<=n;++i){
-		if(col[i]==0&&dfs_cycle(i))
-			break;
-	}
-	if(cycle_hellyea){
-		cout<<"IMPOSSIBLE\n";
-		return 0;
-	}
-	for(int i=1;i<=n;++i){
-		if(vis[i]) continue;
-		dfs(i);
-	}
-	while(st.size()){
-		cout<<st.top()<<" ";
-		st.pop();
-	}
-	cout<<'\n';
-	return 0;
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n,m; cin>>n>>m;
+  vector<vector<int>> adj(n);
+  for(int i=0;i<m;++i){
+    int a,b; cin>>a>>b;
+    --a,--b;
+    adj[a].emplace_back(b);
+  }
+  vector<int> topo_order=topo_sort(n,adj);
+  if(topo_order.size()){
+    for(auto&x:topo_order) cout<<x+1<<" ";
+    cout<<'\n';
+  }else{
+    cout<<"IMPOSSIBLE\n";
+  }
+  return 0;
 }
+
