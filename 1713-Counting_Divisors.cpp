18530dfab721cd56e00bf775c8e47bc238371d2b
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-03-27 14:56:08 +0800

    Solved Counting Divisors

--- 1713-Counting_Divisors.cpp
+++ 1713-Counting_Divisors.cpp
@@ -1,18 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+#define int long long
+int p[1000001];
+signed main(){
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  for(int i=2;i<1000001;++i) if(!p[i]) for(int j=i;j<1000001;j+=i)
+    p[j]=i;
   int n; cin>>n;
-  for(int i=0;i<n;++i){
-    int x; cin>>x;
-    int ans=1;
-    for(int k=2;k*k<=x;++k){
-      int res=1;
-      while(x%k==0)
-        x/=k,++res;
-      ans*=res;
-    }
-    if(x>1) ans*=2;
+  for(;n;--n){
+    int x,ans=1; cin>>x;
+    for(int i=0,res=1,k=p[x];k;++i,ans*=res,res=1,k=p[x]) while(x%k==0)
+      x/=k,++res;
     cout<<ans<<'\n';
   }
   return 0;
